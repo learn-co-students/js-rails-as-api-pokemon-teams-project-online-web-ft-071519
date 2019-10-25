f6c5bf07fbe8982ce05d3caba6d107fea7add25a
Nicole Dow <nikki.dow71@gmail.com>
2019-10-25 18:21:25 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,50 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+const trainersList = document.getElementById("trainers-list")
+
+document.addEventListener("DOMContentLoaded", fetchTrainers());
+
+function fetchTrainers() {
+    fetch(TRAINERS_URL)
+    .then(response => response.json())
+    .then(json => loadTrainers(json))
+}
+
+function loadTrainers(json) {
+    //console.log(json)
+    json.data.forEach(trainer => {
+        const div = document.createElement('div')
+        div.className = 'card'
+        div.id = trainer.attributes.id
+        trainersList.appendChild(div)
+
+        const h2 = document.createElement('h2')
+        h2.innerText = trainer.attributes.name
+        div.appendChild(h2)
+
+        const ul = document.createElement('ul')
+        ul.className = 'pokemons-list'
+        div.appendChild(ul)
+
+        renderPokemons(trainer);
+    })
+}
+
+function renderPokemons(trainer_json){
+    //console.log(trainer_json)
+  trainer_json.attributes.pokemons.forEach(pokemon => {
+    const trainerDiv = document.getElementById(trainer_json.id)
+
+    const li = document.createElement('li')
+    li.innerText = `${pokemon.nickname} (${pokemon.species})`
+    trainerDiv.lastElementChild.appendChild(li)
+
+  })
+}
+
+
+
+
+
